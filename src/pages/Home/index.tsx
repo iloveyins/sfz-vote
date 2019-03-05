02404106b045750f39b17fcfea9cb2b3cda3f220
iloveyins <296394>
2019-03-05 10:15:51 +0800

    home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,25 +22,7 @@ class Home extends React.Component<Iprops>{
                     <a href="#">我要报名</a>
                 </div>
                 <div className="list">
-
-                    <div className="list-item">
-                        <div className="item-content">
-                            <div className="item-img">
-                                <img alt="" src={require('../../static/images/banner.png')} />
-                                <div className="item-title">Top</div>
-                            </div>
-                            <div className="ticket-number">
-                                <span>001号</span>
-                                <span>006票</span>
-                            </div>
-                            <a className="btn-vote" onClick={() => {
-
-                            }}>
-                                为TA投票
-                </a>
-                        </div>
-                    </div>
-
+                    <VoteList />
                 </div>
             </div>
         )
